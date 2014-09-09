07ff95a28a1adcf43c054d1eacc613a42c35cd03
onerebos <onerebos@gmail.com>
2014-09-09 06:46:45 +0000

    rm exvim files

--- .exvim.ucos/update-tags.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.ucos"
-export TOOLS="/home/zswfox/.vim/tools/"
-export CTAGS_CMD="ctags"
-export OPTIONS="--fields=+iaS --extra=+q"
-export TMP="${DEST}/_tags"
-export TARGET="${DEST}/tags"
-sh ${TOOLS}/shell/bash/update-tags.sh
