0180e8e1e6b8b77c189bc7ef5d5116a35f91e3f2
onerebos <onerebos@gmail.com>
2014-09-09 06:44:41 +0000

    rm exvim files

--- .exvim.ucos/update-filelist.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export DEST="./.exvim.ucos"
-export TOOLS="/home/pi/.vim/tools/"
-export FOLDERS="./be,./bsp,./build,./h,./init,./lib,./port,./ucos,./usrApp"
-export FILE_SUFFIXS=".*"
-export TMP="${DEST}/_files"
-export TARGET="${DEST}/files"
-sh ${TOOLS}/shell/bash/update-filelist.sh
